22fca8f3862f7c9ba102c112b590070561309912
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2023-04-21 19:32:51 +0330

    IsSubsequence392

---
+++ Interview.DataStructureAndAlgorithms.LeetCode.Tests/LeetCode75/IsSubsequence392Test.cs
@@ -0,0 +1,18 @@
+﻿using Interview.DataStructureAndAlgorithms.LeetCode.LeetCode75;
+
+namespace Interview.DataStructureAndAlgorithms.LeetCode.Tests.LeetCode75;
+
+public class IsSubsequence392Test
+{
+    [Theory]
+    [InlineData("abc","ahbgdc",true)]
+    [InlineData("axc","ahbgdc",false)]
+    [InlineData("aaaaaa","bbaaaa",false)]
+
+    public void Should_Pass_Input(string s,string t, bool expected)
+    {
+        var algo = new IsSubsequence392();
+        var actual = algo.IsSubsequence(s,t);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Interview.DataStructureAndAlgorithms.LeetCode/LeetCode75/IsSubsequence392.cs
@@ -0,0 +1,39 @@
+﻿namespace Interview.DataStructureAndAlgorithms.LeetCode.LeetCode75;
+
+public class IsSubsequence392
+{
+    /// <summary>
+    /// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+    /// A subsequence of a string is a new string that is formed
+    /// from the original string by deleting some (can be none) of the characters without disturbing
+    /// the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public bool IsSubsequence(string s, string t)
+    {
+        int lastIndex = -1;
+        if (s.Length > t.Length)
+            return false;
+        if (s.Length == t.Length)
+            return s == t;
+        foreach (var chr in s)
+        {
+            if (t.IndexOf(chr,lastIndex+1) is var a && a < 0)
+                return false;
+            else
+            {
+                if (lastIndex > a)
+                    return false;
+                else
+                {
+                    lastIndex = a;
+                }
+            }
+        }
+
+        return true;
+    }
+}
