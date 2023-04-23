8fc971e3b0161697e55f2e0518813cacaa4ca248
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2023-04-23 13:38:07 +0330

    206. Reverse Linked List

---
+++ Interview.DataStructureAndAlgorithms.LeetCode.Tests/LeetCode75/ReverseLinkedList206Test.cs
@@ -0,0 +1,30 @@
+﻿using Interview.DataStructureAndAlgorithms.LeetCode.LeetCode75;
+
+namespace Interview.DataStructureAndAlgorithms.LeetCode.Tests.LeetCode75;
+
+public class ReverseLinkedList206Test
+{
+    [Theory]
+    [ClassData(typeof(ValidCustomerTheory))]
+    public void ReverseList(ReverseLinkedList206.ListNode node)
+    {
+        var algo = new ReverseLinkedList206();
+        var actual = algo.ReverseList(node);
+        Assert.Equal(5,actual.val );
+    }
+    
+    
+
+}
+
+file class ValidCustomerTheory : TheoryData<ReverseLinkedList206.ListNode>
+{
+    public ValidCustomerTheory()
+    {
+        Add(new ReverseLinkedList206.ListNode(1,
+            new ReverseLinkedList206.ListNode(2, 
+                new ReverseLinkedList206.ListNode(3,
+                new ReverseLinkedList206.ListNode(4, 
+                    new ReverseLinkedList206.ListNode(5))))));
+    }
+}
---
+++ Interview.DataStructureAndAlgorithms.LeetCode/LeetCode75/ReverseLinkedList206.cs
@@ -0,0 +1,45 @@
+﻿namespace Interview.DataStructureAndAlgorithms.LeetCode.LeetCode75;
+
+public class ReverseLinkedList206
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head is null)
+        {
+            return null;
+        }
+
+        if (head.next is null)
+        {
+            return head;
+        }
+        var res = ReverseList(head.next,head);
+        return res;
+    }
+
+    public ListNode ReverseList(ListNode head,ListNode prev)
+    {
+        if (head.next is not null)
+        {
+           var res= ReverseList(head.next, head);
+            prev.next = null;
+            head.next = prev;
+            return res;
+        }
+
+        prev.next = null;
+        return new ListNode(head.val, prev);
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
