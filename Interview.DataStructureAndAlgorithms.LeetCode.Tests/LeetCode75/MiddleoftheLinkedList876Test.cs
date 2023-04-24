df5b00982f83a76a34c89856d568d92c15f4f08e
Mostafa Esmaeili <mostafa.esmaeili8@gmail.com>
2023-04-25 00:23:17 +0330

    876. Middle of the Linked List

---
+++ Interview.DataStructureAndAlgorithms.LeetCode.Tests/LeetCode75/MiddleoftheLinkedList876Test.cs
@@ -0,0 +1,23 @@
+﻿using Interview.DataStructureAndAlgorithms.LeetCode.LeetCode75;
+
+namespace Interview.DataStructureAndAlgorithms.LeetCode.Tests.LeetCode75;
+
+public class MiddleoftheLinkedList876Test
+{
+    [Theory]
+    [ClassData(typeof(ValidCustomerTheory))]
+
+    public void MiddleNode(MiddleoftheLinkedList876.ListNode head) {
+        var algo = new MiddleoftheLinkedList876();
+        var actual = algo.MiddleNode(head);
+        Assert.True(true);
+    }
+}
+file class ValidCustomerTheory : TheoryData<MiddleoftheLinkedList876.ListNode>
+{
+    public ValidCustomerTheory()
+    {
+        Add(new MiddleoftheLinkedList876.ListNode(1,
+            new MiddleoftheLinkedList876.ListNode(2)));
+    }
+}
---
+++ Interview.DataStructureAndAlgorithms.LeetCode/LeetCode75/MiddleoftheLinkedList876.cs
@@ -0,0 +1,45 @@
+﻿namespace Interview.DataStructureAndAlgorithms.LeetCode.LeetCode75;
+
+public class MiddleoftheLinkedList876
+{
+    private int count = 1;
+    private int index = 1;
+    private ListNode result = null;
+    public ListNode MiddleNode(ListNode head) {
+
+        if (head.next is not null)
+        {
+            count++;
+            MiddleNode(head.next);
+            if (result is not null)
+            {
+                return result;
+            }
+            if (index == (count - 1))
+            {
+                result = head;
+                return result;
+            }
+            else
+            {
+                count--;
+                return head;
+            }
+            
+        }
+
+        index = (int)(count / 2 + 1);
+        return head;
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
